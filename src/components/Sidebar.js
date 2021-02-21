daedd110fcbc1a928fc9e6e92ce8117dbda77bb9
Dang Nhat Phi <phidnuit@gmail.com>
2021-02-22 00:56:03 +0700

    Fix layout sidebar use ant.design layout.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Layout, Menu } from 'antd';
+import jiraLogo from "./../assets/images/jiraLogo.svg";
+
+const { Sider } = Layout;
 
 export default function Sidebar() {
+  const [state, setState] = useState({
+    collapsed: false,
+  });
+
+  let onCollapse = collapsed => {
+    setState({ collapsed });
+  };
+
   return (
-    <div className="sideBar">
-    <div className="sideBar-top">
-      <div className="sideBar-icon">
-        <i className="fab fa-jira" />
-      </div>
-      <div className="sideBar-icon" data-toggle="modal" data-target="#searchModal" style={{cursor: 'pointer'}}>
-        <i className="fa fa-search" />
-        <span className="title">SEARCH ISSUES</span>
-      </div>
-      <div className="sideBar-icon">
-        <i className="fa fa-plus" />
-        <span className="title">CREATE ISSUES</span>
-      </div>
+    <div>
+        <Sider className="sider-sidebar" collapsible collapsed={state.collapsed} onCollapse={onCollapse}>
+          <div className="logo">
+            <img src={jiraLogo}/>
+          </div>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<i className="fa fa-search fa-lg" ></i>}>
+              <span className="text-uppercase">Search issues</span>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<i className="fa fa-plus fa-lg" ></i>}>
+              <span className="text-uppercase">Create Issue</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
     </div>
-    <div className="sideBar-bottom">
-      <div className="sideBar-icon">
-        <i className="fa fa-question-circle" />
-        <span className="title">ABOUT</span>
-      </div>
-    </div>
-  </div>
   )
 }
