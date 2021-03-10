f43e5135faf4a4a548edbda19029f36934d07ada
Dang Nhat Phi <phidnuit@gmail.com>
2021-03-10 14:57:17 +0700

    Try hard to private route using context api.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Route, Router, Switch } from "react-router-dom";
 import Loading from "./components/loading/Loading";
 import Drawer from "./hocs/Drawer";
@@ -8,14 +8,28 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import ProjectManagement from "./pages/ProjectManagement";
 import SiderTableContent from "./pages/SiderTableContent";
-import { AwesomeBugsTemplate } from "./templates/AwesomeBugsTemplate";
 import { LoginTemplate } from "./templates/LoginTemplate";
 import history from "./util/history";
 import Register from "./pages/Register";
+import { userService } from "./services/UserService";
+import { AuthContext } from "./context/auth";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {  
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    userService.testToken().catch((err) => {
+      if(err.response.data.statusCode == 500) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+  }, []);
+
   return (
-    <>
+    <AuthContext.Provider value={{isLoggedIn, setLoggedIn}}>
       <Router history={history}>
         <Drawer/>
         <Loading/>
@@ -28,14 +42,16 @@ export default function App() {
             LoginComponent={Register}
             SiderTableContentComponent={SiderTableContent}
           />
-            <AwesomeBugsTemplate exact path="/awesomebugs" Component={ProjectDetail}/>
-            <AwesomeBugsTemplate exact path="/createproject" Component={CreateProject}/>
-            <AwesomeBugsTemplate exact path="/" Component={ProjectManagement}/>
-            <AwesomeBugsTemplate exact path="/projectmanagement" Component={ProjectManagement}/>
-            <AwesomeBugsTemplate exact path="/projectdetail/:projectId" Component={ProjectDetail}/>
-            <Route path="*" component={PageNotFound} />
+
+          <PrivateRoute exact path="/awesomebugs" Component={ProjectDetail}/>
+          <PrivateRoute exact path="/createproject" Component={CreateProject}/>
+          <PrivateRoute exact path="/" Component={ProjectManagement}/>
+          <PrivateRoute exact path="/projectmanagement" Component={ProjectManagement}/>
+          <PrivateRoute exact path="/projectdetail/:projectId" Component={ProjectDetail}/>
+          
+          <Route path="*" component={PageNotFound} />
         </Switch>
       </Router>
-    </>
+    </AuthContext.Provider>
   )
 }
--- src/templates/HomeTemplate.js
+++ src/templates/HomeTemplate.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Route } from "react-router-dom";
+// import React from "react";
+// import { Route } from "react-router-dom";
 
-export const HomeTemplate = (props) => {
-  const {Component, ...resParam} = props;
-  return <Route {...resParam} render={(propsRoute) => {
-    return <>
-      <Component {...propsRoute}/>
-    </>
-  }}/>
-}
+// export const HomeTemplate = (props) => {
+//   const {Component, ...resParam} = props;
+//   return <Route {...resParam} render={(propsRoute) => {
+//     return <>
+//       <Component {...propsRoute}/>
+//     </>
+//   }}/>
+// }
